1bdd5bff97cb9a348a021ff56226b5523a87ec0d
Maksim Makarenko <maksmak91@10.211.46.26-vpn.dhcp.yndx.net>
2026-01-08 12:48:22 +0300

    unions migrations

--- migrations/000003_function.down.sql
+++
@@ -1,7 +0,0 @@
-DROP FUNCTION metric.list_metrics();
-
-DROP FUNCTION metric.counters_list_by_metric_names(text[]);
-DROP FUNCTION metric.gauges_list_by_metric_names(text[]);
-
-DROP FUNCTION metric.counters_upsert(json);
-DROP FUNCTION metric.gauges_upsert(json);
